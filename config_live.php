3891722b628cd7057bdd29b9714f8ebbdd2c240f
korkmazfb <32840@ma-web.nl>
2022-06-24 12:00:10 +0200

    lin naar de live server

---
+++ config_live.php
@@ -0,0 +1,11 @@
+<?php
+$config = [
+    'server' => '127.0.0.1',
+    'database' => 'c7059portfolio',
+    'username' => 'c7059portfolio',
+    'password' => 'kU_Tar3Kc9tHT',
+    'port' => 3306
+];
+
+return $config
+?>
